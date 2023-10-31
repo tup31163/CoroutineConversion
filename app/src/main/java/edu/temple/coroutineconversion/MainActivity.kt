eb19ad9033678e240e1e7215816b6ac6cd2f250a
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-10-31 14:09:27 -0400

    Store CoroutineScope in variable, launch onClick

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,11 +27,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         cakeImageView = findViewById(R.id.imageView)
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                reveal()
-            }.start()
+            scope.launch { reveal() }
         }
     }
 
